ba572a13eeb2e1ca03983877b8c01e40a91d9be0
Chahine Chahine <138026169+Chahine-Chahine@users.noreply.github.com>
2024-10-14 12:34:08 +0300

    implemented h2 text styled and finalized the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,6 +11,7 @@ function Hero() {
             </button>
         </nav>
         <h1 className='head_text'>Summarize articles with <br className='max-md:hidden'/> <span className='orange_gradient'>OpenAi GPT-4</span></h1>
+        <h2 className='desc'>Simplify your reading with Summize, an open source article summarizer that transforms lengthly articles into clear and concise summarize</h2>
     </header>
   )
 }
