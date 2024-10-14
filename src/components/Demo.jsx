f5b0c8dcf77e88a680bb6f67c19c27ec6b904bce
Chahine Chahine <138026169+Chahine-Chahine@users.noreply.github.com>
2024-10-14 13:26:45 +0300

    implemented Demo layout and styles with basic usestate

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,17 +2,28 @@ import { useState, useEffect } from "react"
 import { copy, linkIcon , loader , tick } from "../assets"
 
 function Demo() {
+
+    const [article , setarticle] = useState({
+        url: '',
+        summary: '',
+    });
+
+    const handlesubmit = async (e) => {
+        alert("submitted")
+    }
+
   return (
     <section className="mt-16 w-full max-w-xl">
         {/* Search */}
         <div className="flex flex-col w-full gap-2">
-            <form className="relative flex justify-center items-center" onSubmit={()=>{}}>
+            <form className="relative flex justify-center items-center" onSubmit={handlesubmit}>
                 <img src={linkIcon} alt="linkicon" className="absolute left-0 my-2 ml-3 w-5"/>
-                <input type="url" placeholder="Enter a URL" value="" onChange={()=>{}} required className="url_input peer"/>
+                <input type="url" placeholder="Enter a URL" value={article.url} onChange={(e)=>{setarticle({...article,url:e.target.value})}} required className="url_input peer"/>
                 <button type="submit" className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">↵</button>
             </form>
             {/* Browser URL History */}
         </div>
+        {/* Display results */}
     </section>
   )
 }
